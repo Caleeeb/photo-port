a5ccceed93f4023091226e76e3a8f60e1b2c176c
Caleeeb <daycinematography@gmail.com>
2022-07-12 13:20:41 -0400

    nav test adjustment

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,37 +1,51 @@
-// // __tests__/Nav.test.js with hard coded categories
-// import React from 'react';
-// import { render, cleanup } from '@testing-library/react';
-// import '@testing-library/jest-dom/extend-expect';
-// import Nav from '..';
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
-// afterEach(cleanup);
+afterEach(cleanup);
 
-// describe('Nav component', () => {
-//   it('renders', () => {
-//     render(<Nav />);
-//   });
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
-//   it('matches snapshot', () => {
-//     const { asFragment } = render(<Nav />);
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+  })
 
-//     expect(asFragment()).toMatchSnapshot();
-//   });
-// })
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
 
-// describe('emoji is visible', () => {
-//   it('inserts emoji into the h2', () => {
-//     const { getByLabelText } = render(<Nav />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
 
-//     expect(getByLabelText('camera')).toHaveTextContent('📸');
-//   });
-// })
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
 
-// describe('links are visible', () => {
-//   it('inserts text into the links', () => {
-//     const { getByTestId } = render(<Nav />);
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})
 
-//     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-//     expect(getByTestId('about')).toHaveTextContent('About me');
-//   });
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav />);
 
-// })
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
+})
