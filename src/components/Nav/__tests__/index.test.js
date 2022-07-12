b358f7bcabc70fdc9b5ed9288df237520f0f5ff3
Caleeeb <daycinematography@gmail.com>
2022-07-12 11:21:40 -0400

    added new tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,3 +1,4 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -6,15 +7,13 @@ import Nav from '..';
 afterEach(cleanup);
 
 describe('Nav component', () => {
-  // baseline test
   it('renders', () => {
     render(<Nav />);
   });
 
-  //snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
@@ -23,7 +22,6 @@ describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
 })
@@ -31,9 +29,9 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-    // eslint-disable-next-line testing-library/prefer-screen-queries
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
